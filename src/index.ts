5ec5ccc4ff46ef5b412124102cdcf86d52dc5657
ghinaw <h1101241015@student.untan.ac.id>
2026-02-25 04:47:15 +0000

    Pertemuan 4

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ function greet(name: string, umur: number): string {
     return `Halo, ${name}! Umur kamu ${umur} tahun. Selamat belajar TypeScript.`;
 }
 
-const namaMahasiswa = "Budi";
+const namaMahasiswa = "Ghina Audhiya Khairunisa";
 const umur: number = 19;
 
 console.log(greet(namaMahasiswa, umur));
